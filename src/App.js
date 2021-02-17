64fad84ba8fea6ec5accb1edb38f660dd51e70aa
Claudiuts <sarb.claudiu391@gmail.com>
2021-02-16 18:36:27 -0600

    state working

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import './App.css';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+import reducer, { initialState } from './reducers';
+
+
 function App() {
 
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+  // console.log(state);
+  
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -17,10 +23,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
